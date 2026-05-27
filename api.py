461ae9577cf7a714c49788f684d773734e8b397b
Wajiha Abbasi <wajiha.abbasi0405@gmail.com>
2026-05-27 17:26:28 +0200

    created class TAskSTate to manage list of tasks

--- api.py
+++ api.py
@@ -8,10 +8,21 @@ logger.setLevel(logging.INFO) # used to print the output of the agent to the con
 
 class Task:
     class Task:
-    def __init__(self, task_id: int, title: str, time: str):
-        self.id = task_id
-        self.title = title
-        self.time = time
+        def __init__(self, task_id: int, title: str, time: str):
+            self.id = task_id
+            self.title = title
+            self.time = time
 
     def __str__(self):
         return f"[{self.id}] {self.title} at {self.time}"
+    
+class TaskState:
+    def __init__(self):
+        self.tasks = []
+        self.next_id = 1
+
+
+class TaskAssistantFnc(llm.FunctionContext):
+    def __init__(self) -> None:
+        super().__init__()
+        self.state = TaskState()    
