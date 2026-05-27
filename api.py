dee504139b695ff0a38b2558f5114bf28815bb6c
Wajiha Abbasi <wajiha.abbasi0405@gmail.com>
2026-05-27 17:23:54 +0200

    created class Task with id, title and time attribues

--- api.py
+++ api.py
@@ -5,3 +5,13 @@ import logging
 
 logger= logging.getLogger("task")
 logger.setLevel(logging.INFO) # used to print the output of the agent to the console
+
+class Task:
+    class Task:
+    def __init__(self, task_id: int, title: str, time: str):
+        self.id = task_id
+        self.title = title
+        self.time = time
+
+    def __str__(self):
+        return f"[{self.id}] {self.title} at {self.time}"
